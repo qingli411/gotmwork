4a8cb5a6bd58c3090b8dd86f09a8c2efa2c497ad
Qing Li <lqingpku@gmail.com>
2017-10-24 21:18:41 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# gotmwork
+
+This is my [GOTM](http://gotm.net) working directory.
+
+## setup.sh
+
+Bash script to set up the source code, test cases and tools from Github.
---
+++ setup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# This script sets up code, test cases and necessary tools
+# to run GOTM
+#
+# Qing Li, 20171024
+
+function python_version() {
+    # Return 2 if in Python 2.x environment, 3 if in Python 3.x environment
+    python -c "import sys; print(sys.version_info[0])"
+}
+
+function if_python_module() {
+    # Return 1 if the module (name as argument) is installed in the current
+    # Python environment, 0 otherwise
+    python -c "import pkgutil; print(1 if pkgutil.find_loader(\"$1\") else 0)"
+}
+
+# check if in python2 environment
+if [[ $(python_version) == 3 ]]; then
+    echo "Require Python 2.x environment. Stop."
+    exit 1
+fi
+
+# root directory
+gotm_root="${HOME}/models/gotm"
+mkdir -p ${gotm_root}
+cd ${gotm_root}
+
+# github repositories
+gitrep_code="https://github.com/gotm-model/code.git"
+gitrep_cases="https://github.com/gotm-model/cases.git"
+gitrep_editscenario="https://github.com/BoldingBruggeman/editscenario.git"
+gitrep_xmlstore="https://github.com/BoldingBruggeman/xmlstore.git"
+gitrep_xmlplot="https://github.com/BoldingBruggeman/xmlplot.git"
+gitrep_gotmgui="https://github.com/BoldingBruggeman/gotmgui.git"
+
+# clone source code
+for f in code cases
+do
+    # check if installed
+    if [[ -d ${gotm_root}/${f}/.git ]]; then
+        echo "${f} installed. Skip."
+    else
+        # clone source code from github
+        mkdir -p ${f}
+        gvar="gitrep_${f}"
+        gitrep=$(eval echo \$${gvar})
+        echo "Cloning source code from ${gitrep} ..."
+        git clone ${gitrep} ${f}
+    fi
+done
+
+# clone tools
+tools_root="${gotm_root}/tools"
+mkdir -p ${tools_root}
+cd ${tools_root}
+for f in editscenario xmlstore xmlplot gotmgui
+do
+    # check if installed
+    if [[ $(if_python_module ${f}) ]]; then
+        echo "${f} installed. Skip."
+    else
+        # clone source code from github
+        mkdir -p ${f}
+        gvar="gitrep_${f}"
+        gitrep=$(eval echo \$${gvar})
+        echo "Cloning source code from ${gitrep} ..."
+        git clone ${gitrep} ${f}
+        # install tool
+        cd ${f}
+        pip wheel .
+        pip install ${f}.*.whl
+        cd ${tools_root}
+    fi
+done
