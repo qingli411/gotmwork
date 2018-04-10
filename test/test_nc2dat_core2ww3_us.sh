ae3a5e40c1361fa1e8361bf1c95adff30aeb39f4
Qing Li <lqingpku@gmail.com>
2018-04-10 15:29:46 -0400

    Update nc2dat_core2ww3 to prepare significant wave height data for Langmuir mixing parameterization when Stokes drift profile is calculated from the partitioned surface Stokes drift; update kpp.nml

--- test/test_nc2dat_core2ww3_us.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-../tools/nc2dat_core2ww3_us -i "/Volumes/Qing_Work/data/WAVEWATCH_COREII/ww3.2009_usp.nc" -o "usp_file.dat" -lat 25.7 -lon 180.2 -ds 20090325 -de 20090411
-
-
-
