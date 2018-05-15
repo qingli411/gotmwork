93948c07b386e5222a4e20ef7c13b040d1da93d9
qingli411 <lqingpku@gmail.com>
2018-05-15 12:48:30 -0400

    Set up COREII runs

--- test/test_plot_map.sh
+++ test/test_plot_map.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../tools/plot_map -f COREII -t OSMOSIS -a mldMean -m deltaR -vmax 0 -vmin -200 -ds 20080615 -de 20091231 -ds_a 20090101 -de_a 20090331
-../tools/plot_map -f COREII -t OSMOSIS -a mldMean -m maxNsqr -vmax 0 -vmin -200 -ds 20080615 -de 20091231 -ds_a 20090101 -de_a 20090331
+turbmethod="KPP-CVMix"
+../tools/plot_map -f COREII -t ${turbmethod} -a mldMean -m deltaR -vmax 0 -vmin -200 -ds 20080615 -de 20091231 -ds_a 20090101 -de_a 20090331 -dir_in /Users/qingli/scratch/VR5m_DT60s/${turbmethod}
