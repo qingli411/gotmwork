132f900e99a44697a18fccb602393c9688c02746
Brandon Reichl <brandon.reichl@noaa.gov>
2018-05-17 04:05:32 -0700

    Adding several updates
      - Add Idealized_Hurricane testcase
      - Add Idealized_Tests testcase
      - Add an environment file (enviro_file) to read GOTM location and work location and etc. from.
      - Add new methods to tools/case_turbmethods

--- tools/case_turbmethod.sh
+++ tools/case_turbmethod.sh
@@ -23,24 +23,54 @@ case ${turbmethod} in
     "KPP-CVMix")
         ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 99
         ${cmd_nmlchange} -f kpp.nml -e lcvmix -v .true.
+        ${cmd_nmlchange} -f gotmmean.nml -e lagrangian_mixing -v .false.
+        ${cmd_nmlchange} -f gotmmean.nml -e stokes_coriolis -v .false.
         ;;
     "KPP-GOTM")
         ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 99
         ${cmd_nmlchange} -f kpp.nml -e lcvmix -v .false.
+        ${cmd_nmlchange} -f gotmmean.nml -e lagrangian_mixing -v .false.
+        ${cmd_nmlchange} -f gotmmean.nml -e stokes_coriolis -v .false.
         ;;
     "KPPLT-EFACTOR")
         ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 99
         ${cmd_nmlchange} -f kpp.nml -e lcvmix -v .true.
         ${cmd_nmlchange} -f kpp.nml -e langmuir_method -v 1
+        ${cmd_nmlchange} -f gotmmean.nml -e lagrangian_mixing -v .false.
+        ${cmd_nmlchange} -f gotmmean.nml -e stokes_coriolis -v .false.
         ;;
     "KPPLT-ENTR")
         ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 99
         ${cmd_nmlchange} -f kpp.nml -e lcvmix -v .true.
         ${cmd_nmlchange} -f kpp.nml -e langmuir_method -v 2
+        ${cmd_nmlchange} -f gotmmean.nml -e lagrangian_mixing -v .false.
+        ${cmd_nmlchange} -f gotmmean.nml -e stokes_coriolis -v .false.
+        ;;
+    "KPPLT-RWHGK")
+        ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 99
+        ${cmd_nmlchange} -f kpp.nml -e lcvmix -v .true.
+        ${cmd_nmlchange} -f kpp.nml -e langmuir_method -v 3
+        ${cmd_nmlchange} -f gotmmean.nml -e lagrangian_mixing -v .true.
+        ${cmd_nmlchange} -f gotmmean.nml -e stokes_coriolis -v .true.
         ;;
     "EPBL")
         ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 100
-	    ${cmd_nmlchange} -f MOMturb.nml -e Mode -v 0
+        ${cmd_nmlchange} -f MOMturb.nml -e Mode -v 0
+        ;;
+    "EPBL-LT")
+        ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 100
+        ${cmd_nmlchange} -f MOMturb.nml -e mode -v 0
+        ${cmd_nmlchange} -f MOMturb.nml -e efactor_method -v 1
+        ${cmd_nmlchange} -f MOMturb.nml -e langmuir_method -v 1
+        ${cmd_nmlchange} -f epbl.nml -e lt_enhance_form -v 3
+        ;;
+    "K-EPSILON-SG")
+        ${cmd_nmlchange} -f gotmturb.nml -e turb_method -v 2
+        ${cmd_nmlchange} -f gotmturb.nml -e tke_method -v 2
+        ${cmd_nmlchange} -f gotmturb.nml -e len_scale_method -v 8
+        ${cmd_nmlchange} -f gotmturb.nml -e stab_method -v 3
+        ${cmd_nmlchange} -f gotmmean.nml -e lagrangian_mixing -v .false.
+        ${cmd_nmlchange} -f gotmmean.nml -e stokes_coriolis -v .false.
         ;;
     *)
         echo "Turbulence method ${turbmethod} not supported. Stop."
