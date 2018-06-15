9b72c6eb8cc7576d83ab7b99bb1ed12569416973
Qing Li <lqingpku@gmail.com>
2018-06-15 10:26:17 -0400

    Use ensemble mean as reference in TEST_RES/plot_pfl_cmp_turbmethods.py

--- cases/TEST_RES/plot_pfl_cmp_turbmethods.py
+++ cases/TEST_RES/plot_pfl_cmp_turbmethods.py
@@ -11,8 +11,10 @@ from testrestool import *
 
 def main():
 
+    l_ens = True
     # loop over all cases
     nc = len(case_list)
+    print(nc)
     nv = len(pfl_list)
     nm = len(turbmethod_list)
     for i in np.arange(nc):
@@ -24,7 +26,10 @@ def main():
             c_max = pfl_cmax_list[j,i]
             c_min = pfl_cmin_list[j,i]
             d_max = pfl_dmax_list[j,i]
-            plot_pfl_cmp_turbmethods(turbmethod_list, case, var, c_max, c_min, d_max, depth)
+            if l_ens:
+                plot_pfl_cmp_turbmethods_ens(turbmethod_list, case, var, c_max, c_min, d_max, depth)
+            else:
+                plot_pfl_cmp_turbmethods(turbmethod_list, case, var, c_max, c_min, d_max, depth)
 
 def plot_pfl_cmp_turbmethods(turbmethod_list, case, var, c_max, c_min, d_max, depth):
 
@@ -103,5 +108,90 @@ def plot_pfl_cmp_turbmethods(turbmethod_list, case, var, c_max, c_min, d_max, de
     # close figure
     plt.close()
 
+def plot_pfl_cmp_turbmethods_ens(turbmethod_list, case, var, c_max, c_min, d_max, depth):
+
+    # input data directory
+    dataroot = dir_in+'/'+case
+
+    # output figure name
+    figdir = dir_out+'/'+case
+    os.makedirs(figdir, exist_ok=True)
+    figname = figdir+'/Pfl_cmp_turbmethods_ens_'+var+'.png'
+
+    # number of turbulence methods
+    nm = len(turbmethod_list)
+
+    # use ensemble mean as the reference
+    for i in np.arange(nm):
+        data0 = dataroot+'/'+turbmethod_list[i]+'_VR1m_DT60s/gotm_out.nc'
+        # read data
+        infile0 = Dataset(data0, 'r')
+        if i == 0:
+            fld0, z0 = read_pfl(infile0, var)
+        else:
+            tmp0, z0 = read_pfl(infile0, var)
+            fld0 = fld0 + tmp0
+    # get the mean
+    fld0 = fld0/nm
+    nctime0 = infile0.variables['time']
+    t_cal = 'standard'
+    dttime0 = num2date(nctime0[:], units=nctime0.units, calendar=t_cal)
+
+    # figure
+    fig_width = 6
+    fig_height = 2+2*nm
+
+    # plot figure
+    f, axarr = plt.subplots(nm+1, sharex=True)
+    f.set_size_inches(fig_width, fig_height)
+
+    # contour levels
+    c_int = (c_max-c_min)/20
+    levels0 = np.arange(c_min, c_max+c_int, c_int)
+    d_int = d_max/10
+    levels1 = np.arange(-d_max, d_max+d_int, d_int)
+
+    # contourf plot
+    im0 = axarr[0].contourf(dttime0, z0, np.transpose(fld0), levels0, extend='both', cmap='rainbow')
+    axarr[0].set_ylabel('Depth (m)')
+    axarr[0].set_ylim([depth, 0])
+    title0 = case+' '+var+' Ens. Mean'
+    axarr[0].set_title(title0, fontsize=10)
+    cb0 = plt.colorbar(im0, ax=axarr[0])
+    cb0.formatter.set_powerlimits((-2, 2))
+    cb0.update_ticks()
+
+    # loop over turbmethods
+    for i in np.arange(nm):
+        j = i+1
+        data1 = dataroot+'/'+turbmethod_list[i]+'_VR1m_DT60s/gotm_out.nc'
+        infile1 = Dataset(data1, 'r')
+        fld1, z1 = read_pfl(infile1, var)
+        nctime1 = infile1.variables['time']
+        dttime1 = num2date(nctime1[:], units=nctime1.units, calendar=t_cal)
+
+        im1 = axarr[j].contourf(dttime1, z1, np.transpose(fld1-fld0), levels1, extend='both', cmap='RdBu_r')
+        axarr[j].set_ylabel('Depth (m)')
+        axarr[j].set_ylim([depth, 0])
+        title1 = 'Diff. '+turbmethod_list[i]+' $-$ Ens. Mean'
+        axarr[j].set_title(title1, fontsize=10)
+        cb1 = plt.colorbar(im1, ax=axarr[j])
+        cb1.formatter.set_powerlimits((-2, 2))
+        cb1.update_ticks()
+
+    # auto adjust the x-axis label
+    plt.gcf().autofmt_xdate()
+
+    # reduce margin
+    plt.tight_layout()
+
+    # save figure
+    plt.savefig(figname, dpi = 300)
+
+    # close figure
+    plt.close()
+
+if __name__ == "__main__":
+    main()
 if __name__ == "__main__":
     main()
