87501a52d9fa2f6f524db07ee1eb2d00965b13c9
Qing Li <lqingpku@gmail.com>
2018-11-26 09:18:49 -0700

    Add map diagnostics SST and SSS

--- tools/gotmanalysis.py
+++ tools/gotmanalysis.py
@@ -1052,7 +1052,9 @@ class GOTMOutputDataMap(object):
         """
         switcher = {
                 'mld_deltaR_mean': self.mean_state_timeseries(var='mld_deltaR', **kwargs),
-                'PE_delta': self.delta_timeseries(var='PE', **kwargs)
+                'PE_delta': self.delta_timeseries(var='PE', **kwargs),
+                'SST': self.mean_state_timeseries(var='sst', **kwargs),
+                'SSS': self.mean_state_timeseries(var='sss', **kwargs),
                 }
         if list_keys:
             return list(switcher.keys())
