bd59e6f8e696559a0bb5e023e6e5e43d8ba90721
Qing Li <lqingpku@gmail.com>
2019-01-25 13:52:10 -0700

    Clean up visualization

--- tools/gotmanalysis.py
+++ tools/gotmanalysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import matplotlib.cm as cm
+from matplotlib.colors import from_levels_and_colors
 from netCDF4 import Dataset, num2date
 from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.basemap import shiftgrid
@@ -1246,7 +1248,200 @@ class GOTMOutputDataMap(object):
 # Functions
 #--------------------------------
 
-def plot_forcing_regime(f_regime, axis=None, add_colorbar=True, **kwargs):
+def plot_dist_3p(hst, xi, yi, axis=None, filled=False, fcolors=None, **kwargs):
+    """Plot bi-dimensional histogram. Show the contours of the
+       histogram which enclose the highest 30%, 60%, and 90%
+       centered distribution.
+
+    :his: (2D numpy array) bi-dimensional histogram
+    :xi: (1D numpy array) centers of x dimension
+    :yi: (1D numpy array) centers of y dimension
+    :axis: (matplotlib.axes, optional) axis to plot figure on
+    :filled: (bool) filled contour if True
+    :fcolors: (list, optional) color string or sequence of colors, optional)
+    :return: (matplotlib figure object) figure
+
+    """
+    vl = [0.3, 0.6, 0.9]
+    fig = plot_dist_xp(hst, xi, yi, axis=axis, levels=vl, filled=filled, fcolors=fcolors, **kwargs)
+    return fig
+
+def plot_dist_4p(hst, xi, yi, axis=None, filled=False, fcolors=None, **kwargs):
+    """Plot bi-dimensional histogram. Show the contours of the
+       histogram which enclose the highest 30%, 60%, 90% and 99%
+       centered distribution.
+
+    :his: (2D numpy array) bi-dimensional histogram
+    :xi: (1D numpy array) centers of x dimension
+    :yi: (1D numpy array) centers of y dimension
+    :axis: (matplotlib.axes, optional) axis to plot figure on
+    :filled: (bool) filled contour if True
+    :fcolors: (list, optional) color string or sequence of colors, optional)
+    :return: (matplotlib figure object) figure
+
+    """
+    vl = [0.3, 0.6, 0.9, 0.99]
+    fig = plot_dist_xp(hst, xi, yi, axis=axis, levels=vl, filled=filled, fcolors=fcolors, **kwargs)
+    return fig
+
+def plot_dist_xp(hst, xi, yi, axis=None, levels=None, filled=False, fcolors=None, **kwargs):
+    """Plot bi-dimensional histogram. Show the contours of the
+       histogram which enclose the highest p1%, p2%, ... and pN%
+       centered distribution.
+
+    :his: (2D numpy array) bi-dimensional histogram
+    :xi: (1D numpy array) centers of x dimension
+    :yi: (1D numpy array) centers of y dimension
+    :axis: (matplotlib.axes, optional) axis to plot figure on
+    :levels: (list of float, optional) contour levels, 0.0-1.0
+    :filled: (bool) filled contour if True
+    :fcolors: (list, optional) color string or sequence of colors
+    :return: (matplotlib figure object) figure
+
+    """
+    # use curret axis if not specified
+    if axis is None:
+        axis = plt.gca()
+    hsum = np.sum(hst)
+    hlist = -np.sort(-hst.flatten())/hsum
+    hcum = np.cumsum(hlist)
+    vl = levels
+    nv = len(vl)
+    vlev = np.zeros(nv)
+    for i in np.arange(nv):
+        ind = np.argmin(abs(hcum-vl[i]))
+        vlev[i] = hlist[ind]
+    pdfData = hst/hsum
+    pdfData[pdfData==0] = 1e-12
+    if not filled:
+        fig = axis.contour(xi, yi, np.log10(np.transpose(pdfData)), levels=np.log10(vlev[::-1]), **kwargs)
+    else:
+        if fcolors is None:
+            cmap = cm.get_cmap('bone')
+            fcolors = cmap(np.linspace(1.0, 0.0, 11)[0:nv+1])
+        else:
+            nfc = len(fcolors)
+            if nfc != nv+1:
+                raise ValueError('Length of fcolors should equal to number of levels + 1.')
+        fig = axis.contourf(xi, yi, np.log10(np.transpose(pdfData)), levels=np.log10(vlev[::-1]),
+                            colors=fcolors, extend='both', **kwargs)
+    return fig
+
+
+def plot_regime_diagram_background_BG12(axis=None):
+    """Plot the background of the regime diagram
+       following Fig. 3 of Belcher et al., 2012
+
+    :axis: (matplotlib.axes, optional) axis to plot figure on
+
+    """
+    if axis is None:
+        axis = plt.gca()
+
+    # range of power
+    xpr = [-1, 1]
+    ypr = [-3, 3]
+    # range
+    xlims = [10**i for i in xpr]
+    ylims = [10**i for i in ypr]
+    # size of x and y
+    nx = 500
+    ny = 500
+    xx = np.logspace(xpr[0], xpr[1], nx)
+    yy = np.logspace(ypr[0], ypr[1], ny)
+    zz1 = np.zeros([nx, ny])
+    zz2 = np.zeros([nx, ny])
+    zz3 = np.zeros([nx, ny])
+    for i in np.arange(nx):
+        for j in np.arange(ny):
+            zz1[i,j] = 2*(1-np.exp(-0.5*xx[i]))
+            zz2[i,j] = 0.22*xx[i]**(-2)
+            zz3[i,j] = 0.3*xx[i]**(-2)*yy[j]
+    zz = zz1 + zz2 + zz3
+    axis.contourf(xx, yy, np.transpose(np.log10(zz)),
+                  levels=[-0.1, 0, 0.1, 0.25, 0.5, 1, 2, 3],
+                  cmap='summer', extend='both')
+    axis.contour(xx, yy, np.transpose(np.log10(zz)),
+                  levels=[-0.1, 0, 0.1, 0.25, 0.5, 1, 2, 3],
+                  colors='darkgray')
+    axis.contour(xx, yy, np.transpose(zz1/zz), levels=0.9, colors='k',
+                linestyles='-', linewidths=2)
+    axis.contour(xx, yy, np.transpose(zz2/zz), levels=0.9, colors='k',
+                linestyles='-', linewidths=2)
+    axis.contour(xx, yy, np.transpose(zz3/zz), levels=0.9, colors='k',
+                linestyles='-', linewidths=2)
+    axis.set_xlim(xlims)
+    axis.set_ylim(ylims)
+    axis.set_xscale('log')
+    axis.set_yscale('log')
+    axis.set_xlabel('La$_t$')
+    axis.set_ylabel('$h/L_L$')
+    axis.set_aspect(aspect=1/3)
+    axis.text(0.11, 4e-3, 'Langmuir', bbox=dict(boxstyle="square",ec='k',fc='w'))
+    axis.text(3, 4e-3, 'Wind', bbox=dict(boxstyle="square",ec='k',fc='w'))
+    axis.text(0.13, 1e2, 'Convection', bbox=dict(boxstyle="square",ec='k',fc='w'))
+
+
+def plot_regime_diagram_background_L19(axis=None):
+    """Plot the background of the reegime diagram
+       in Li et al., 2019
+
+    :axis: (matplotlib.axes, optional) axis to plot figure on
+
+    """
+    if axis is None:
+        axis = plt.gca()
+    # range of power
+    xpr = [-1, 1]
+    ypr = [-3, 3]
+    # range
+    xlims = [10**i for i in xpr]
+    ylims = [10**i for i in ypr]
+    # background following Fig. 3 of Belcher et al., 2012
+    nx = 500
+    ny = 500
+    xx = np.logspace(xpr[0], xpr[1], nx)
+    yy = np.logspace(ypr[0], ypr[1], ny)
+    zz1 = np.zeros([nx, ny])
+    zz2 = np.zeros([nx, ny])
+    zz3 = np.zeros([nx, ny])
+    for i in np.arange(nx):
+        for j in np.arange(ny):
+            zz1[i,j] = 2*(1-np.exp(-0.5*xx[i]))
+            zz2[i,j] = 0.22*xx[i]**(-2)
+            zz3[i,j] = 0.3*xx[i]**(-2)*yy[j]
+    zz = zz1 + zz2 + zz3
+
+    rz_ST = zz1/zz
+    rz_LT = zz2/zz
+    rz_CT = zz3/zz
+    fr = np.ones(zz.shape) * 7
+    cfrac = 0.25
+    fr[(rz_LT<cfrac) & (rz_CT<cfrac)] = 1
+    fr[(rz_ST<cfrac) & (rz_CT<cfrac)] = 2
+    fr[(rz_ST<cfrac) & (rz_LT<cfrac)] = 3
+    fr[(rz_ST>=cfrac) & (rz_LT>=cfrac) & (rz_CT<cfrac)] = 4
+    fr[(rz_ST>=cfrac) & (rz_CT>=cfrac) & (rz_LT<cfrac)] = 5
+    fr[(rz_LT>=cfrac) & (rz_CT>=cfrac) & (rz_ST<cfrac)] = 6
+
+    color_list = ['firebrick','forestgreen','royalblue','gold','orchid','turquoise','w']
+    cb_ticks = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5]
+    cmap, norm = from_levels_and_colors(cb_ticks, color_list)
+    axis.contourf(xx, yy, np.transpose(fr), cmap=cmap, norm=norm)
+    axis.contour(xx, yy, np.transpose(fr), colors='darkgray')
+    axis.set_xlim(xlims)
+    axis.set_ylim(ylims)
+    axis.set_xscale('log')
+    axis.set_yscale('log')
+    axis.set_xlabel('La$_t$')
+    axis.set_ylabel('$h/L_L$')
+    axis.set_aspect(aspect=1/3)
+    axis.text(0.11, 4e-3, 'Langmuir', bbox=dict(boxstyle="square",ec='k',fc='w'))
+    axis.text(3, 4e-3, 'Wind', bbox=dict(boxstyle="square",ec='k',fc='w'))
+    axis.text(0.13, 1e2, 'Convection', bbox=dict(boxstyle="square",ec='k',fc='w'))
+
+
+def plot_forcing_regime_map(f_regime, axis=None, add_colorbar=True, **kwargs):
     """Plot forcing regime in scatters on a map
 
     :f_regime: (GOTMMap object) forcing regime
